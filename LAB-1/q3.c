8e5b44619015ec8591bcc46f9c1da6e1afd578d3
Nikunj Singh <nikunj.singh95@gmail.com>
2017-11-13 11:07:18 +0530

    Added q3.c

---
+++ LAB-1/q3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	// 1st part  
+	int fd;
+	char *f1;
+	char *buf = "abcde";
+	char *new = "12345";
+	int size = strlen(buf);
+	if(argc < 2) {
+		printf("Insufficient arguments\n");
+		return -1;
+	}
+	f1 = argv[1];
+	fd = creat(f1, S_IRWXU);
+	write(fd, buf, size);
+	close(fd);
+
+	// 2nd part
+	fd = open(f1, O_WRONLY | O_APPEND);
+	if(fd == -1) {
+		perror("Operation Unsuccessful\n");
+		return -1;
+	}
+	// 3rd part
+	lseek(fd, 0, SEEK_SET);
+	// 4th part
+	size = strlen(new);
+	write(fd, new, size);
+	close(fd);
+	return 0;
+}
