1f008189e8f6ccdfe43bc4993456b6d4767bcdbc
AbhijeetKale <abhinitinkale@gmail.com>
2017-11-13 12:53:41 +0530

    LAB 10.3 complete

--- LAB-10/3.c
+++ LAB-10/3.c
@@ -5,77 +5,78 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <unistd.h>
-#define SIZE sizeof(int)
-void reader(key_t key, int shmid) {
-	int *data, semid;
-	data = (int *) shmat(shmid, 0, 0);
-	semid = semget(key, 1, IPC_EXCL | 0666);
-	printf("trying to lock....\n");
+#include <limits.h>
+#define SIZE 4
+#define ITERATION 10
+union semun {
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+	struct seminfo *__buf;
+};
+int toggle_sem(int semid, int sem_no, int flag, int sem_op) {
 	struct sembuf sb;
-	sb.sem_num = 0;
-	sb.sem_op = -1;  /* set to allocate resource */
-	sb.sem_flg = SEM_UNDO;
-	while(1) {
-		sleep(1);
-		if (semop(semid, &sb, 1) == -1) {
-			 perror("semop");
-		}
-		else
-			printf("Locked.\n");
+	sb.sem_num = sem_no;
+	sb.sem_op = sem_op;
+	sb.sem_flg = flag;
+	return semop(semid, &sb, 1);
+}
+void reader(key_t key, int shmid) {
+	int *data, semid, count;
+	data = (int *) shmat(shmid, 0, 0);
+	semid = semget(key, 3, IPC_EXCL | 0666);
+	if(semid == -1) {
+		perror("semget failed ");
+		exit(1);
+	}
+	for(count = 0; count < ITERATION; count++) {
+		toggle_sem(semid, 2, 0, -1); //wait till the buffer has at least one element
+		toggle_sem(semid, 0, 0, -1); //lock on shared memory
+		printf("Data consumed = %d\n", data[count % SIZE]);
+		toggle_sem(semid, 0, 0, 1);
+		toggle_sem(semid, 1, 0, 1);
 	}
 }
 void producer(key_t key, int shmid) {
-	int *data;
-	data = (int *) shmat(shmid, 0, SHM_RDONLY);
-	int semid = semget(key, 1, IPC_EXCL | 0666);
+	int *data, count;
+	data = (int *) shmat(shmid, 0, 0);
+	int semid = semget(key, 3, IPC_EXCL | 0666);
 	if(semid == -1) {
 		perror("Semget failed");
 		exit(1);
 	}
-	printf("Trying to unlock...\n");
-	struct sembuf sb;
-	sb.sem_num = 0;
-	sb.sem_op = 1; /* free resource */
-	while(1) {
-		sleep(1);
-		if (semop(semid, &sb, 1) == -1) {
-			perror("semop");
-			exit(1);
-		}
-		else
-			printf("Unlocked\n");
+	for(count = 0; count < ITERATION; count++) {
+		toggle_sem(semid, 1, 0, -1);		//wait until the buffer has at least one empty space
+		toggle_sem(semid, 0, 0, -1);		//Lock on shared memory
+		data[count % SIZE] = count;
+		printf("Data produced = %d\n", data[count % SIZE]);
+		toggle_sem(semid, 0, 0, 1);
+		toggle_sem(semid, 2, 0, 1);
 	}
 }
 int main(int argc, char *argv[]) {
 	int shmid, pid, semid;
-	struct sembuf sb;
 	key_t key = 5683;
-	shmid = shmget(key, SIZE, IPC_CREAT | 0600);
+	shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0600);
 	if(shmid == -1) {
 		perror("Error while creating Shared memeory: ");
 		return 1;
 	}
-	semid = semget(key, 1, IPC_EXCL | 0666);
+	semid = semget(key, 3, IPC_EXCL | IPC_CREAT | 0666);
 	if(semid >= 0) {
-		union semum {
-			int val;
-			struct semid_ds *buf;
-			ushort *array;
-		}arg;
+		union semun arg;
+		arg.val = 1;
+		semctl(semid, 0, SETVAL, arg);	//Semaphore for buffer
+		arg.val = SIZE;
+		semctl(semid, 1, SETVAL, arg); //number of empty elements in buffer
 		arg.val = 0;
-		semctl(semid, 0, SETVAL, arg);
-		sb.sem_num = 0;
-        	sb.sem_op = 1;
-        	sb.sem_flg = 0;
-        	semop(semid, &sb, 1);
+		semctl(semid, 2, SETVAL, arg); //number of filled elements in buffer
 		pid = fork();
 		if(pid == 0) {
-			printf("Inside child\n");
 			reader(key, shmid);
 		}
 		else if(pid != -1) {
-			printf("Inside parent\n");
-		//	producer(key, shmid);
+			producer(key, shmid);
 		}
 		else {
 			perror("Fork failed: ");
@@ -84,7 +85,7 @@ int main(int argc, char *argv[]) {
 	else {
 		perror("Faled to get semaphore ");
 	}
-//	semctl(semid, 0, IPC_RMID);
+	semctl(semid, 0, IPC_RMID);
 	shmctl(shmid, 0, IPC_RMID);
 	return 0;
 }
