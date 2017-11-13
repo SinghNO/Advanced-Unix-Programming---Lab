d93a39bcd833c32505247b40c6946fb6a7b51a6a
Nikunj Singh <nikunj.singh95@gmail.com>
2017-11-13 11:10:12 +0530

    Added q1.c

---
+++ LAB-4/q1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+void printAll();
+void addNew(char *name, char *value);
+
+int main(int argc, char *argv[]) {
+	if(argc < 3) {
+		// .o, name, value
+		printf("Insufficient arguments\n");
+		return -1;
+	}
+	printAll();
+	printf("-----NEW ADDITION CORRESPONDING TO COMMAND LINE INPUT----\n");
+	addNew(argv[1], argv[2]);
+	printAll();
+	return 0;
+}
+
+void printAll() {
+	int i = 0;
+	for(i = 0; environ[i] != NULL; i++) {
+		printf("%s\n", environ[i]);
+	}
+	return;
+}
+
+void addNew(char *name, char *value) {
+	// rewrite bit is set
+	setenv(name, value, 1);
+	return;
+}
